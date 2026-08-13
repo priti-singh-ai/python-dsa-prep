003e1812efe158f57068b65b76b23bbf740efa40
priti-singh-ai <pritiex6@gmail.com>
2026-08-13 14:19:34 +0530

    starting array again

--- arrays/Top K frequents.py
+++ arrays/Top K frequents.py
@@ -18,17 +18,18 @@ Space Complexity: O(n)
 """
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums)+1)]
+        count = {} #count
+        freq = [[] for i in range(len(nums)+1)] #frequency array
 
-        for n in nums:
-            count[n]= 1+count.get(n,0)
-        for n,c in count.items():
-            freq[c].append(n)
+        for num in nums:
+            count[num] = 1+ count.get(num,0) #calculate count of each number
+        for num, cnt in count.items():
+            freq[cnt].append(num) #calculate frequency of each number
 
         res = []
-        for j in range(len(freq)-1,0,-1):
-            for num in freq[j]:
-                res.append(num)
-                if len(res) == k:
+        for i in range(len(freq)-1,0,-1): #from the last value of frequency index
+            for num in freq[i]: #iterate to each freq value
+                res.append(num) #append it to result
+                if len(res) == k: #once result and k len is equal
                     return res
+
---
+++ tree/same_tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    """Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+    Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.
+    Difficulty: Easy
+    Pattern: Depth first search, breadth first search, binary tree
+
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+
+
+"""
+    def isSameTree(self, p:Optional[TreeNode], q:Optional[TreeNode]) -> bool:
+
+        def dfs(p,q):
+            if not p and not q:
+                return True
+            elif not p or not q or p.val != q.val:
+                return False
+            return dfs(p.left,q.left) and dfs(p.right,q.right)
+
+        return dfs(p,q)
